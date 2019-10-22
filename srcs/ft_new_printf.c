48984c6ae012ab87eb11aa20f441ea9795d823f5
Lucas MOULIN <lmoulin@e2r2p1.42.fr>
2019-10-22 06:28:22 +0200

    flags *. good a priori

--- srcs/ft_new_printf.c
+++ srcs/ft_new_printf.c
@@ -35,9 +35,7 @@ t_count		ft_arg_int(va_list aux, t_count cmp)
 	int		nb;
 
 	nb = va_arg(aux, int);
-//	cmp.len = ft_strlen_nb(nb, 10);
 	cmp.str = ft_itoa((int)nb);
-//	printf("%s\n", cmp.str);
 	return (cmp);
 }
 
@@ -166,10 +164,8 @@ int		ft_convertible(char c)
 t_count		ft_flag_star(va_list aux, const char *s, t_count cmp)
 {
 	int		k;
-	int		j;
 
 	k = cmp.i + 2;
-	j = 0;
 	if (s[k] == '.')
 	{
 		if (ft_convertible(s[k + 1]) == 1)
@@ -198,16 +194,19 @@ t_count		ft_flag_point(va_list aux, t_count cmp, const char *s)
 	if (s[k] == '*')
 	{
 		if (ft_convertible(s[k + 1]) == 1)
+		{
 			cmp.zero = va_arg(aux, int);
-			cmp.i = k;
+			cmp.i += 2;
+		}
 	}
-	else if (s[k] >= '0' && s[k] <= '9')
+	else if ((s[k] >= '0' && s[k] <= '9') || ft_convertible(s[k]) == 1)
 	{
+		cmp.i = (s[k] >= '0' && s[k] <= '9') ? k : cmp.i + 1;
 		while (s[k] >= '0' && s[k] <= '9')
 			k++;
-	if (k > cmp.i + 2 && ft_convertible(s[k]) == 1)
-		cmp.zero = ft_atoi((char *)&s[cmp.i + 1]);
-		cmp.i = k;
+		if (ft_convertible(s[k]) == 1)
+			cmp.zero = ft_atoi((char *)&s[cmp.i]);
+		cmp.i = k - 1;
 	}
 	return (cmp);
 }
@@ -299,25 +298,18 @@ t_count		ft_print_arg(t_count cmp)
 
 	if ((cmp.zero -= ft_strlen(cmp.str)) < 0)
 		cmp.zero = 0;
-//	printf("%lu len\n", cmp.str);
 	neg = (cmp.space > 0) ? 1 : -1;
 	cmp.space = (neg == -1) ? -cmp.space : cmp.space;
-//	printf("space = %d\n", cmp.space);
-//	printf("zero = %d\n", cmp.zero);
-//	printf("%ld len\n", ft_strlen(cmp.str));
 	cmp.space -= ft_strlen(cmp.str);
 	cmp.space -= cmp.zero;
-//	printf("normal space = %ld\n", cmp.space - ft_strlen(cmp.str) - cmp.zero); 
 	cmp.space = (cmp.space > 0) ? cmp.space : 0;
 	cmp.len = cmp.len + ft_strlen(cmp.str) + cmp.space + cmp.zero;
 	cmp = ft_print_front_flag(cmp, neg);
-//	printf("strlen = %zu, cmp.len = %d cmp.space = %d, cmp.zero = %d\n", ft_strlen(cmp.str), cmp.len, cmp.space, cmp.zero);
 //	while (cmp.space-- > 0)
 //		ft_putchar_fd(' ', 1);
 //	while (cmp.zero > 0)
 //		ft_putchar_fd('0', 1);
 	ft_putstr_fd(cmp.str, 1);
-//	printf("%d spaaaace\n", cmp.space);
 	cmp = ft_print_back_flag(cmp, neg);
 	cmp.zero = 0;
 	cmp.space = 0;
@@ -327,7 +319,7 @@ t_count		ft_print_arg(t_count cmp)
 
 t_count		ft_check(va_list aux, const char *s, t_count cmp)
 {
-//	printf("%d = i1\n", cmp.i);
+//	printf("\n%d = i1\n", cmp.i);
 	cmp = ft_flags(aux, cmp, s[cmp.i + 1], (char *)s);
 //	printf("%d = i2\n", cmp.i);
 	if (s[cmp.i + 1] == 'd' || s[cmp.i + 1] == 'i')
@@ -393,14 +385,14 @@ int		ft_printf(const char *str, ...)
 	return (cmp.len);
 }
 
-/*
+
 int main(int argc, const char *argv[])
 {
 	argc = 4;
 
-//	printf("return value : %d", printf("%*d\n", 12, 1000));
+	printf("return value : %d", printf("%*.d\n", 300, 36));
 //	printf("\n\n\n");
-	printf("return value : %d", ft_printf("%*d\n", 12, 1000));
+	printf("return value : %d", ft_printf("%*.d\n", 300, 36));
 
 return 0;
-}*/
+}
